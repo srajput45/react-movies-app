398ecdd65dce883b153849dc1fba3f71f86baa4d
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-05-30 23:27:05 +0530

    Added 'Movie Name' filter on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,6 +6,12 @@ import moviesData from '../../common/movieData';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
 const styles = theme => ({
     root: {
@@ -22,10 +28,31 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
-    }
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+     },
+     title: {
+        color: theme.palette.primary.light,
+     }
  });
 
 class Home extends Component {
+    constructor(){
+        super();
+        this.state = {
+            movieName : ""
+        }
+    }
+    movieNameChangeHandler = (e) =>{
+        this.setState({movieName: e.target.value})
+    }
     render() {
         const { classes } = this.props;
         return (
@@ -34,14 +61,44 @@ class Home extends Component {
                 <div className={classes.upcomingMoviesHeading}>
                     <span>Upcoming Movies</span>
                 </div>
-                <GridList cols={5} className={classes.gridListUpcomingMovies}>
+                <GridList cols={5}  className={classes.gridListUpcomingMovies}>
                     {moviesData.map(movies =>(
                        <GridListTile key={movies.id}>
-                           <img src={movies.poster_url} atl={movies.title}/>
+                           <img src={movies.poster_url} className="movie-poster" atl={movies.title}/>
                            <GridListTileBar title={movies.title} />
                        </GridListTile> 
                     ))}
                 </GridList>
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                        <Card>
+                            <CardContent>
+                                <FormControl className={classes.formControl}>
+                                    <Typography className={classes.title} color="textSecondary">
+                                        FIND MOVIES BY:
+                                    </Typography>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                                    <Input id="movieName" type='text' onChange={this.movieNameChangeHandler} />
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
