43b9e0808d70f94047364cf2f3f4db98ab9fb697
srajput45 <43792808+srajput45@users.noreply.github.com>
2020-05-31 13:39:13 +0530

    Added ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,9 +1,11 @@
 import React , {Component} from 'react';
 import './Details.css';
+import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
 import Typography from '@material-ui/core/Typography';
 import { Link } from '@material-ui/core';
+import Home from '../home/Home'
  
 class Details extends Component{
     constructor() {
@@ -20,11 +22,19 @@ class Details extends Component{
         this.setState({ currentState });
         console.log(this.state);
     }
+    backToHomeHandler = () =>{
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
     render(){
         let movie = this.state.movie;
         return(
-            <div>
+            <div className="details">
                 <Header />
+                <div className="back">
+                    <Typography onClick={this.backToHomeHandler}>
+                       &#60; Back to Home
+                    </Typography>
+                </div>
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
                         <img src={movie.poster_url} alt={movie.title} />
